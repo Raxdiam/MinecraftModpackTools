f490a5be91d273ba4af28dc1449ccada5a17a32f
Raxdiam <Raxdiam@users.noreply.github.com>
2023-08-06 15:11:26 -0400

    Rename LocalModData to IndexFile and update

---
+++ MCMPTools/IndexFile.cs
@@ -0,0 +1,28 @@
+﻿namespace MCMPTools;
+
+public class IndexFile
+{
+    public string FileName { get; set; }
+    public bool Disabled { get; set; }
+    public IndexFileCurse Curse { get; set; }
+    public IndexFileModrinth Modrinth { get; set; }
+}
+
+public class IndexFileCurse
+{
+    public uint FileId { get; set; }
+    public uint ProjectId { get; set; }
+}
+
+public class IndexFileModrinth
+{
+    public string ModId { get; set; }
+    public string Version { get; set; }
+}
+
+public enum HashFormat
+{
+    Sha1,
+    Sha256,
+    Sha512
+}
