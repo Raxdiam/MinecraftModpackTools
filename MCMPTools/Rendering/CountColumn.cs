b4a3266b43facefd9ddf28023c3a1105cb15a8a8
Raxdiam <Raxdiam@users.noreply.github.com>
2023-08-06 15:13:34 -0400

    Create count progress bar column

---
+++ MCMPTools/Rendering/CountColumn.cs
@@ -0,0 +1,12 @@
+﻿using Spectre.Console;
+using Spectre.Console.Rendering;
+
+namespace MCMPTools.Rendering;
+
+public sealed class CountColumn : ProgressColumn
+{
+    public override IRenderable Render(RenderOptions options, ProgressTask task, TimeSpan deltaTime)
+    {
+        return new Markup($"{task.Value}[grey]/[/]{task.MaxValue}");
+    }
+}
